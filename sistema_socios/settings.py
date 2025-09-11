a33d31541d40d8c0f8823a610aca5fa2077da4eb
Maxi <maximiliano.perez16@inacapmail.cl>
2025-09-10 22:52:28 -0300

    Cambios en settings

--- sistema_socios/settings.py
+++ sistema_socios/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'sistema_socios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'sistemadb',
+        'USER': 'root',
+        'PASSWORD':'',
+        'OPTIONS':{
+            'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'",
+            'charset':'utf8mb4'
+        }
     }
 }
 
