5b67a65a822cc7273e251292c530feeb4a31915f
Pepe <jose.valdebenito24@inacapmail.cl>
2025-10-07 00:14:36 -0300

    confirm

--- sistemaApp/views.py
+++ sistemaApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from sistemaApp.models import Proveedores
+from sistemaApp.forms import ProveedoresForm
 
 # Create your views here.
 def inicio(request):
@@ -13,12 +14,36 @@ def proveedores(request):
     return render(request, 'sistema/proveedores.html', data)
 
 def crearProveedores(request):
+    form = ProveedoresForm()
+    data = {
+        'titulo':'Crear Proveedor',
+        'form':form, 
+        'ruta':'/producto/proveedores/'
+    }
     if request.method == 'POST':
-        form = ProveedoresForm(request.POST, request.FILES)
+        form = ProveedoresForm(request.POST,request.FILES)
+        if form.is_valid():
+            FileSystemStorage(location='media/proveedores/')
+            form.save()
+            messages.success(request,'Proveedor creado con éxito.')
+    return render(request,'producto/createF.html',data)
+
+def editarProveedor(request,id):
+    prov = Proveedores.objects.get(pk=id)
+    form = ProveedoresForm(instance=prov)
+    data = {
+        'titulo':'Editar Proveedor',
+        'form':form, 
+        'ruta':'/producto/proveedores/'
+    }
+    if request.method == 'POST':
+        form = ProveedoresForm(request.POST,request.FILES,instance=prov)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Proveedor creado exitosamente.')
-            return redirect('proveedores')
-    else:
-        form = ProveedoresForm()
-    return render(request, 'sistema/crear_proveedor.html', {'form': form})
+            messages.success(request,'Proveedor editado con éxito.')
+    return render(request,'producto/createF.html',data)
+
+def eliminarProveedor(request,id):
+    prov = Proveedores.objects.get(pk=id)
+    prov.delete()
+    return redirect('/producto/proveedores')
