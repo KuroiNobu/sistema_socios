13b85eb2c73ece9e42b337abe4d458e7d798406d
Pepe <jose.valdebenito24@inacapmail.cl>
2025-10-07 01:24:48 -0300

    yes

--- sistemaApp/views.py
+++ sistemaApp/views.py
@@ -11,14 +11,14 @@ def inicio(request):
 def proveedores(request):
     proveedores = Proveedores.objects.all()
     data =  {'titulo': 'Lista de Proveedores', 'proveedores': proveedores}
-    return render(request, 'sistema/proveedores.html', data)
+    return render(request, 'sistemas/proveedores.html', data)
 
 def crearProveedores(request):
     form = ProveedoresForm()
     data = {
         'titulo':'Crear Proveedor',
         'form':form, 
-        'ruta':'/producto/proveedores/'
+        'ruta':'/sistemas/proveedores/'
     }
     if request.method == 'POST':
         form = ProveedoresForm(request.POST,request.FILES)
@@ -26,7 +26,7 @@ def crearProveedores(request):
             FileSystemStorage(location='media/proveedores/')
             form.save()
             messages.success(request,'Proveedor creado con éxito.')
-    return render(request,'producto/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarProveedor(request,id):
     prov = Proveedores.objects.get(pk=id)
@@ -34,32 +34,32 @@ def editarProveedor(request,id):
     data = {
         'titulo':'Editar Proveedor',
         'form':form, 
-        'ruta':'/producto/proveedores/'
+        'ruta':'/sistemas/proveedores/'
     }
     if request.method == 'POST':
         form = ProveedoresForm(request.POST,request.FILES,instance=prov)
         if form.is_valid():
             form.save()
             messages.success(request,'Proveedor editado con éxito.')
-    return render(request,'producto/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarProveedor(request,id):
     prov = Proveedores.objects.get(pk=id)
     prov.delete()
-    return redirect('/producto/proveedores')
+    return redirect('/sistemas/proveedores')
 
 
 def descuentos(request):
     descuentos = Descuentos.objects.all()
     data =  {'titulo': 'Lista de Descuentos', 'descuentos': descuentos}
-    return render(request, 'sistema/descuentos.html', data)
+    return render(request, 'sistemas/descuentos.html', data)
 
 def crearDescuentos(request):
     form = DescuentosForm()
     data = {
         'titulo':'Crear Descuento',
         'form':form, 
-        'ruta':'/producto/descuentos/'
+        'ruta':'/sistemas/descuentos/'
     }
     if request.method == 'POST':
         form = DescuentosForm(request.POST,request.FILES)
@@ -67,7 +67,7 @@ def crearDescuentos(request):
             FileSystemStorage(location='media/descuentos/')
             form.save()
             messages.success(request,'Descuento creado con éxito.')
-    return render(request,'producto/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarDescuentos(request,id):
     desc = Descuentos.objects.get(pk=id)
@@ -75,32 +75,32 @@ def editarDescuentos(request,id):
     data = {
         'titulo':'Editar Descuentos',
         'form':form, 
-        'ruta':'/producto/descuentos/'
+        'ruta':'/sistemas/descuentos/'
     }
     if request.method == 'POST':
         form = DescuentosForm(request.POST,request.FILES,instance=desc)
         if form.is_valid():
             form.save()
             messages.success(request,'Descuento editado con éxito.')
-    return render(request,'producto/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarDescuentos(request,id):
     desc = Descuentos.objects.get(pk=id)
     desc.delete()
-    return redirect('/producto/descuentos')
+    return redirect('/sistemas/descuentos')
 
 
 def usuarios(request):
     usuarios = Usuarios.objects.all()
     data =  {'titulo': 'Lista de Usuarios', 'usuarios': usuarios}
-    return render(request, 'sistema/usuarios.html', data)
+    return render(request, 'sistemas/usuarios.html', data)
 
 def crearUsuarios(request):
     form = UsuariosForm()
     data = {
         'titulo':'Crear Usuario',
         'form':form, 
-        'ruta':'/sistema/usuarios/'
+        'ruta':'/sistemas/usuarios/'
     }
     if request.method == 'POST':
         form = UsuariosForm(request.POST,request.FILES)
@@ -108,7 +108,7 @@ def crearUsuarios(request):
             FileSystemStorage(location='media/usuarios/')
             form.save()
             messages.success(request,'Usuario creado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarUsuarios(request,id):
     user = Usuarios.objects.get(pk=id)
@@ -116,31 +116,31 @@ def editarUsuarios(request,id):
     data = {
         'titulo':'Editar Usuario',
         'form':form, 
-        'ruta':'/sistema/usuarios/'
+        'ruta':'/sistemas/usuarios/'
     }
     if request.method == 'POST':
         form = UsuariosForm(request.POST,request.FILES,instance=user)
         if form.is_valid():
             form.save()
             messages.success(request,'Usuario editado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarUsuarios(request,id):
     user = Usuarios.objects.get(pk=id)
     user.delete()
-    return redirect('/sistema/usuarios')
+    return redirect('/sistemas/usuarios')
 
 def credenciales(request):
     credenciales = Credenciales.objects.all()
     data =  {'titulo': 'Lista de Credenciales', 'credenciales': credenciales}
-    return render(request, 'sistema/credenciales.html', data)
+    return render(request, 'sistemas/credenciales.html', data)
 
 def crearCredenciales(request):
     form = CredencialesForm()
     data = {
         'titulo':'Crear Credencial',
         'form':form, 
-        'ruta':'/sistema/credenciales/'
+        'ruta':'/sistemas/credenciales/'
     }
     if request.method == 'POST':
         form = CredencialesForm(request.POST,request.FILES)
@@ -148,7 +148,7 @@ def crearCredenciales(request):
             FileSystemStorage(location='media/credenciales/')
             form.save()
             messages.success(request,'Credencial creada con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarCredenciales(request,id):
     cred = Credenciales.objects.get(pk=id)
@@ -156,31 +156,31 @@ def editarCredenciales(request,id):
     data = {
         'titulo':'Editar Credencial',
         'form':form, 
-        'ruta':'/sistema/credenciales/'
+        'ruta':'/sistemas/credenciales/'
     }
     if request.method == 'POST':
         form = CredencialesForm(request.POST,request.FILES,instance=cred)
         if form.is_valid():
             form.save()
             messages.success(request,'Credencial editada con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarCredenciales(request,id):
     cred = Credenciales.objects.get(pk=id)
     cred.delete()
-    return redirect('/sistema/credenciales')
+    return redirect('/sistemas/credenciales')
 
 def socios(request):
     socios = Socios.objects.all()
     data =  {'titulo': 'Lista de Socios', 'socios': socios}
-    return render(request, 'sistema/socios.html', data)
+    return render(request, 'sistemas/socios.html', data)
 
 def crearSocios(request):
     form = SociosForm()
     data = {
         'titulo':'Crear Socio',
         'form':form, 
-        'ruta':'/sistema/socios/'
+        'ruta':'/sistemas/socios/'
     }
     if request.method == 'POST':
         form = SociosForm(request.POST,request.FILES)
@@ -188,7 +188,7 @@ def crearSocios(request):
             FileSystemStorage(location='media/socios/')
             form.save()
             messages.success(request,'Socio creado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarSocios(request,id):
     socio = Socios.objects.get(pk=id)
@@ -196,31 +196,31 @@ def editarSocios(request,id):
     data = {
         'titulo':'Editar Socio',
         'form':form, 
-        'ruta':'/sistema/socios/'
+        'ruta':'/sistemas/socios/'
     }
     if request.method == 'POST':
         form = SociosForm(request.POST,request.FILES,instance=socio)
         if form.is_valid():
             form.save()
             messages.success(request,'Socio editado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarSocios(request,id):
     socio = Socios.objects.get(pk=id)
     socio.delete()
-    return redirect('/sistema/socios')
+    return redirect('/sistemas/socios')
 
 def pagos(request):
     pagos = Pagos.objects.all()
     data =  {'titulo': 'Lista de Pagos', 'pagos': pagos}
-    return render(request, 'sistema/pagos.html', data)
+    return render(request, 'sistemas/pagos.html', data)
 
 def crearPagos(request):
     form = PagosForm()
     data = {
         'titulo':'Crear Pago',
         'form':form, 
-        'ruta':'/sistema/pagos/'
+        'ruta':'/sistemas/pagos/'
     }
     if request.method == 'POST':
         form = PagosForm(request.POST,request.FILES)
@@ -228,7 +228,7 @@ def crearPagos(request):
             FileSystemStorage(location='media/pagos/')
             form.save()
             messages.success(request,'Pago creado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarPagos(request,id):    
     pago = Pagos.objects.get(pk=id)
@@ -236,31 +236,31 @@ def editarPagos(request,id):
     data = {
         'titulo':'Editar Pago',
         'form':form, 
-        'ruta':'/sistema/pagos/'
+        'ruta':'/sistemas/pagos/'
     }
     if request.method == 'POST':
         form = PagosForm(request.POST,request.FILES,instance=pago)
         if form.is_valid():
             form.save()
             messages.success(request,'Pago editado con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarPagos(request,id):
     pago = Pagos.objects.get(pk=id)
     pago.delete()
-    return redirect('/sistema/pagos')
+    return redirect('/sistemas/pagos')
 
 def cuotas(request):
     cuotas = Cuotas.objects.all()
     data =  {'titulo': 'Lista de Cuotas', 'cuotas': cuotas}
-    return render(request, 'sistema/cuotas.html', data)
+    return render(request, 'sistemas/cuotas.html', data)
 
 def crearCuotas(request):
     form = CuotasForm()
     data = {
         'titulo':'Crear Cuota',
         'form':form, 
-        'ruta':'/sistema/cuotas/'
+        'ruta':'/sistemas/cuotas/'
     }
     if request.method == 'POST':
         form = CuotasForm(request.POST,request.FILES)
@@ -268,7 +268,7 @@ def crearCuotas(request):
             FileSystemStorage(location='media/cuotas/')
             form.save()
             messages.success(request,'Cuota creada con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def editarCuotas(request,id):
     cuota = Cuotas.objects.get(pk=id)
@@ -276,16 +276,16 @@ def editarCuotas(request,id):
     data = {
         'titulo':'Editar Cuota',
         'form':form, 
-        'ruta':'/sistema/cuotas/'
+        'ruta':'/sistemas/cuotas/'
     }
     if request.method == 'POST':
         form = CuotasForm(request.POST,request.FILES,instance=cuota)
         if form.is_valid():
             form.save()
             messages.success(request,'Cuota editada con éxito.')
-    return render(request,'sistema/createF.html',data)
+    return render(request,'sistemas/createF.html',data)
 
 def eliminarCuotas(request,id):
     cuota = Cuotas.objects.get(pk=id)
     cuota.delete()
-    return redirect('/sistema/cuotas')
+    return redirect('/sistemas/cuotas')
